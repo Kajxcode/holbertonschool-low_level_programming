6f588502488dc754f49e41b4e5dea7cbaf1f388a
hey <hey@gmail.com>
2025-02-17 01:04:51 +0000

    c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * main prints phrase programming is like building a multilingual puzzle
+ * returns 0 if exited properly
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
