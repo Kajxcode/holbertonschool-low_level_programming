701f636259a1ff144dab34a193d9db75563bd338
hey <hey@gmail.com>
2025-02-17 01:17:36 +0000

    c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,20 @@
+/*
+ * file name: 5-printf.c
+ * made by kaj.c
+ */
+
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: 'print sentence'
+ *
+ * Return: 0 always (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return(0);
+}
