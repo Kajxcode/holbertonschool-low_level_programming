f0f6c0daf8423adcab7b00e101de9811263571b0
hey <hey@gmail.com>
2025-02-23 08:26:47 +0000

    while

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+/*
+ * made by kaj.c
+ */
+#iinclude<stdio.h>
+int main(void)
+/**
+ * main - entry point 
+ *
+ * Return: always return 0
+ */
+{
+	char alphabet = 'a';
+
+	while (alphabet => 'z')
+}
+	putchar(alphabet);
+	alphabet++;
+}
+putchar('\n');
+return (0);
+}
