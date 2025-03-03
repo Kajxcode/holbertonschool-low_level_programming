5188a674aa4992d80e388b94d55db77ad01e789b
Kajxcode <kajkkennedy@gmail.com>
2025-03-03 14:16:24 +1100

    Update 9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,15 +10,33 @@
 */
 void times_table(void)
 {
-	int i, j;
+	int x, y, z, u, d;
 
-	for (i = 0; i <= 9; i++)
+	for (x = 0; x <= 9; x++)
 	{
-		printf("%d, ", i * 0);
-		for (j = 1; j <= 8; j++)
+		for (y = 0; y <= 9; y++)
 		{
-			printf("%2d, ", i * j);
+			z = x * y;
+			if (z > 9)
+			{
+				u = z % 10;
+				d = (z - u) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
+			}
+			else
+			{
+				if (y != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(z + '0');
+			}
 		}
-		printf("%2d\n", i * j);
+		_putchar('\n');
 	}
 }
