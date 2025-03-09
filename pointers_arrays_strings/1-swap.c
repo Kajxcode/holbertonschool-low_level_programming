6ce5cf61028211efe3b7c9ca7e81b788c576c5b7
CISCO2020 <CISCO2020@GG-MACHINE>
2025-03-09 13:46:22 +1100

    fix

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -5,7 +5,7 @@
 * @a: first pointer
 * @b: second pointer
 */
-void swap_int(int* a, int* b)
+void swap_int(int *a, int *b)
 {
 	int i = *a;
 	*a = *b;
--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -5,7 +5,7 @@
 * @s: data from usr
 * Return: length of string
 */
-int _strlen(char* s)
+int _strlen(char *s)
 {
 	int i = 0, length = 0;
 
