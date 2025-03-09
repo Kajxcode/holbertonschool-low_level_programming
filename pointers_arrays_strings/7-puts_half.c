33cffb90144fc82cc08b9b8670354f0ad40803b6
CISCO2020 <CISCO2020@GG-MACHINE>
2025-03-09 16:09:17 +1100

    fix

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -4,12 +4,12 @@
 void puts_half(char *str)
 {
 	int len = 0;
+	int mid = len / 2;
+
 	while (str[len] != '\0')
 	{
 		len++;
 	}
-	int mid = len / 2;
-
 	while (str[mid] != '\0')
 	{
 		write(1, &str[mid], 1);
