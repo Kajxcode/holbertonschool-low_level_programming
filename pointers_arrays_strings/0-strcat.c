eea3cc067acb5d593a2cfdb0cb52b1133f37d7f6
CISCO2020 <CISCO2020@GG-MACHINE>
2025-03-11 12:09:38 +1100

    add

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* _strcat - appends src to dest string
+* @dest: dest to append by src
+* @src: string to append to dest
+* Return: address of dest
+*/
+char *_strcat(char *dest, char *src)
+{
+    char *ptr = dest;
+
+    while (*ptr)
+        ptr++;
+
+    while ((*ptr++ = *src++));
+
+    return dest;
+}
