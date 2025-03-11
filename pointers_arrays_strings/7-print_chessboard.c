f27aac18e34ff39bf394b3ab79a0cd6e805f86de
CISCO2020 <CISCO2020@GG-MACHINE>
2025-03-11 20:15:10 +1100

    fx

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -6,14 +6,14 @@
  */
 void print_chessboard(char (*a)[8])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 8; i++) /* Loop through rows */
-    {
-        for (j = 0; j < 8; j++) /* Loop through columns */
-        {
-            _putchar(a[i][j]); /* Print each chess piece */
-        }
-        _putchar('\n'); /* Newline after each row */
-    }
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
 }
--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -11,9 +11,9 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i < size; i++)
 {
-		sum1 += a[i * size + i];         /* Primary diagonal */
-		sum2 += a[i * size + (size - 1 - i)]; /* Secondary diagonal */
+		sum1 += a[i * size + i];
+		sum2 += a[i * size + (size - 1 - i)];
 }
 
- printf("%d, %d\n", sum1, sum2);
+printf("%d, %d\n", sum1, sum2);
 }
