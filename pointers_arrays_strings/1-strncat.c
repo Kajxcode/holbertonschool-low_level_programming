757f4a0636318c71580485eb5c60f4a9d7ef9e7c
CISCO2020 <CISCO2020@GG-MACHINE>
2025-03-11 16:31:59 +1100

    f

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -6,17 +6,15 @@
 * @n: number of bytes appended to dest
 * Return: ptr to dest
 */
-char* _strncat(char* dest, char* src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
-	char* ptr = dest;
-
+	char *ptr = dest;
+	
 	while (*ptr)
 		ptr++;
-}
 
 	while (*src && n--)
 		*ptr++ = *src++;
-}
 
-	return (dest);
+return (dest);
 }
