58a4f604d2b01d3ae8de2f94f5fafc00fa6fd72f
CISCO2020 <CISCO2020@GG-MACHINE>
2025-03-13 16:02:38 +1100

    a

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -4,11 +4,11 @@
  * _print_rev_recursion - Prints a string in reverse recursively
  * @s: The string to be printed
  */
-void print_rev_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 			return;
 
-	print_rev_recursion(s + 1);
+	_print_rev_recursion(s + 1);
 		_putchar(*s);
 }
