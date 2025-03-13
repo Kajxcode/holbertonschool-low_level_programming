b0ebc466c8b1237d4096c9200235e6bc973b9bf4
CISCO2020 <CISCO2020@GG-MACHINE>
2025-03-13 16:16:18 +1100

    d

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -7,13 +7,13 @@
  */
 int is_prime_helper(int n, int divisor)
 {
-    if (divisor * divisor > n)
-        return (1);
+	if (divisor * divisor > n)
+		return (1);
 
-    if (n % divisor == 0)
-        return (0);
+	if (n % divisor == 0)
+		return (0);
 
-    return is_prime_helper(n, divisor + 1);
+	return (is_prime_helper(n, divisor + 1));
 }
 
 /**
@@ -23,8 +23,8 @@ int is_prime_helper(int n, int divisor)
  */
 int is_prime_number(int n)
 {
-    if (n < 2)
-        return (0);
+	if (n < 2)
+		return (0);
 
-    return (is_prime_helper(n, 2));
+	return (is_prime_helper(n, 2));
 }
