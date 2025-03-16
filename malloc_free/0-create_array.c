fd9e7fafbb0a5053a568af3ec63a8abf9ff3cb11
CISCO2020 <CISCO2020@GG-MACHINE>
2025-03-16 13:18:30 +1100

    a

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -3,7 +3,7 @@
  * create_array - Creates an array of chars and initializes it with c
  * @size: The size of the array to create
  * @c: The character to initialize the array with
- * Return: Pointer to the array, or NULL if size is 0 or memory allocation fails
+ * Return: Pointer to the array, NULL if size is 0
  */
 char *create_array(unsigned int size, char c)
 {
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	arr = (char*)malloc(size * sizeof(char));
+	arr = (char *)malloc(size * sizeof(char));
 	if (arr == NULL)
 		return (NULL);
 
