05e0af025093285d42c0b7018dcbe91267d9e2b5
CISCO2020 <CISCO2020@GG-MACHINE>
2025-03-17 16:23:07 +1100

    add

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - free dog
+ * @d: dog
+ * Return: void
+ */
+void free_dog(dog_t* d)
+{
+	if (d == 0)
+		return;
+	if (d->name)
+		free(d->name);
+	if (d->owner)
+		free(d->owner);
+	free(d);
+}
