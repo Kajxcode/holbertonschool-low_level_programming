b3b70a1a9449b9f67e51eb6c6599b69570eb11c0
CISCO2020 <CISCO2020@GG-MACHINE>
2025-03-17 15:52:38 +1100

    a

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 /**
  * new_dog - Creates a new dog struct and copies name and owner
@@ -8,6 +7,30 @@
  * @age: age of dog
  * @owner: owner name
  */
+int _strlen(char *s)
+{
+	int i = 0, length = 0;
+
+	while (s[i++])
+	{
+		length++;
+	}
+	return (length);
+}
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
@@ -18,27 +41,27 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (name != NULL)
 	{
-		d->name = malloc(strlen(name) + 1);
+		d->name = malloc(_strlen(name) + 1);
 		if (d->name == NULL)
 		{
 			free(d);
 			return (NULL);
 		}
-		strcpy(d->name, name);
+		_strncpy(d->name, name);
 	}
 	else
 		d->name = NULL;
 
 	if (owner != NULL)
 	{
-		d->owner = malloc(strlen(owner) + 1);
+		d->owner = malloc(_strlen(owner) + 1);
 		if (d->owner == NULL)
 		{
 			free(d->name);
 			free(d);
 			return (NULL);
 		}
-		strcpy(d->owner, owner);
+		_strncpy(d->owner, owner);
 	}
 	else
 		d->owner = NULL;
