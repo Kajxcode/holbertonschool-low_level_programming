b51c0c417f54a499812350167f9d7f31aad2de2f
CISCO2020 <CISCO2020@GG-MACHINE>
2025-03-17 15:46:14 +1100

    fx

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,45 +3,47 @@
 #include <string.h>
 #include "dog.h"
 /**
-* new_dog
-*/
+ * new_dog - Creates a new dog struct and copies name and owner
+ * @name: anem of dog
+ * @age: age of dog
+ * @owner: owner name
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d = malloc(sizeof(dog_t));
+	dog_t *d;
 
+	d = malloc(sizeof(dog_t));
 	if (d == NULL)
-	{
 		return (NULL);
-	}
-	{
+
 	if (name != NULL)
-		{
-			d->name = malloc(strlen(name) + 1);
-			if (d->name == NULL)
-			{
-				free(d);
-				return (NULL);
-			}
-	strcpy(d->name, name);
-		}
-	else
 	{
-		d->name = NULL;
+		d->name = malloc(strlen(name) + 1);
+		if (d->name == NULL)
+		{
+			free(d);
+			return (NULL);
+		}
+		strcpy(d->name, name);
 	}
+	else
+		d->name = NULL;
+
 	if (owner != NULL)
 	{
 		d->owner = malloc(strlen(owner) + 1);
-			if (d->owner == NULL)
-			{
-				free(d->name);
-				free(d);
-				return (NULL);
-			}
+		if (d->owner == NULL)
+		{
+			free(d->name);
+			free(d);
+			return (NULL);
+		}
 		strcpy(d->owner, owner);
 	}
 	else
-	{
-		d->age = age;
+		d->owner = NULL;
 
-		return (d);
-	}
+	d->age = age;
+
+	return (d);
+}
