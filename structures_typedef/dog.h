206049a5d55d3929ad417d16c014980507162edc
CISCO2020 <CISCO2020@GG-MACHINE>
2025-03-17 14:02:02 +1100

    add

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG
+#define DOG
+
+/**
+* struct dog- new type of data for dogs
+* @name: name of dog
+* @age: age of dog
+* @owner: owner name
+*/
+
+struct dog {
+	char *name;
+	float *age;
+	owner *char;
+};
+#endif
