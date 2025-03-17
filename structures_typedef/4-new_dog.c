3632ee1a9b4079c59a5183e8323b5dcab0462b5b
CISCO2020 <CISCO2020@GG-MACHINE>
2025-03-17 16:21:46 +1100

    a

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -19,9 +19,9 @@ void _strcopy(char *dst, char *src)
 /**
 * new_dog - creates a new dog
 * @name: dog name
-* @age : dog age
-* @owner : owner name
-* Return : pointer to the new dog
+* @age: dog age
+* @owner: owner name
+* Return: pointer to the new dog
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
