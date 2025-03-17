3dba0578e19337ff7b58ad583e198eba87431c2c
CISCO2020 <CISCO2020@GG-MACHINE>
2025-03-17 14:26:59 +1100

    fx

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "dog.h"
-
+/**
+* init_dog - inits a variable of type dog
+* @d: dog id
+* @name: name of dog
+* @age: age of dog
+* @owner: owner name
+*/
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
