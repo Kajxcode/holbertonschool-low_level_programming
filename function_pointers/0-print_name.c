dbd90b2d0e84e43813edd1bd04a2269e02494e58
CISCO2020 <CISCO2020@GG-MACHINE>
2025-03-18 11:20:47 +1100

    fx

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -6,7 +6,7 @@
 * @f: pointer to functionthat takes a char * argument
 * Return: nothing
 */
-void print_name(char* name, void (*f)(char*))
+void print_name(char *name, void (*f)(char*))
 {
 	if (name != NULL && f != NULL)
 		f(name);
