34f0be90aee05264af64f2f857252824a8b8591e
CISCO2020 <CISCO2020@GG-MACHINE>
2025-03-19 13:47:44 +1100

    add

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "function_pointers"
+/**
+* int_index - searches for an integer
+* @array: ptr to array
+* @size: num of elements in array
+* @cmp: funciton pointer
+* Return: int or -1
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if ((*cmp)(array[i]))
+			return (i);
+	}
+	return(-1);
+}
