e30cfc473811f2c2d636ec44a1e488f8acbbd166
CISCO2020 <CISCO2020@GG-MACHINE>
2025-03-22 14:39:22 +1100

    ffx

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,9 +1,24 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+
+/**
+ * struct tdisplay - Format specifier and display function pair
+ * @format_type: Format specifier character ('c', 'i', 'f', 's')
+ * @fdisplay: Pointer to the display function
+ */
+
+typedef struct tdisplay
+{
+	char format_type;
+	void (*fdisplay)(va_list);
+} tdisplay;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+int _putchar(char c);
 
 #endif
