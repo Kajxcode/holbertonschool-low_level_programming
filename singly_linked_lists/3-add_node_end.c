e42a562e762237f8d051e7c520d47a309e175e59
CISCO2020 <CISCO2020@GG-MACHINE>
2025-04-03 14:09:23 +1100

    fdx

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -10,42 +10,42 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node, *temp;
-    char *dup_str;
-    unsigned int len = 0;
+	list_t *new_node, *temp;
+	char *dup_str;
+	unsigned int len = 0;
 
-    if (!head || !str)
-        return (NULL);
+	if (!head || !str)
+		return (NULL);
 
-    while (str[len])
-        len++;
+	while (str[len])
+		len++;
 
-    new_node = malloc(sizeof(list_t));
-    if (!new_node)
-        return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
 
-    dup_str = strdup(str);
-    if (!dup_str)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	dup_str = strdup(str);
+	if (!dup_str)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    new_node->str = dup_str;
-    new_node->len = len;
-    new_node->next = NULL;
+	new_node->str = dup_str;
+	new_node->len = len;
+	new_node->next = NULL;
 
-    if (!*head)
-    {
-        *head = new_node;
-        return (new_node);
-    }
+	if (!*head)
+	{
+		*head = new_node;
+		return (new_node);
+	}
 
-    temp = *head;
-    while (temp->next)
-        temp = temp->next;
+	temp = *head;
+	while (temp->next)
+		temp = temp->next;
 
-    temp->next = new_node;
+	temp->next = new_node;
 
-    return (new_node);
+	return (new_node);
 }
