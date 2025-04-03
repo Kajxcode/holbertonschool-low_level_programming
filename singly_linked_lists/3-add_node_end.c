c80d1e41715c6515c913ca705bd738e39d451f05
CISCO2020 <CISCO2020@GG-MACHINE>
2025-04-03 14:03:32 +1100

    fx

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -2,40 +2,45 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-
 /**
  * add_node_end - Adds a new node at the end of a list_t list
  * @head: A pointer to the pointer to the head node
  * @str: The string to be added as the new node's value
  * Return: The address of the new element, or NULL if it failed
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node;
-    unsigned int len = 0;
+    list_t *new_node, *temp;
+    char *dup_str;
 
-    char *dup_str = strdup(str);
-
-    if (!dup_str)
+    if (!head || !str)
         return (NULL);
 
-    while (dup_str[len])
-        len++;
-
     new_node = malloc(sizeof(list_t));
-
     if (!new_node)
+        return (NULL);
+
+    dup_str = strdup(str);
+    if (!dup_str)
     {
-        free(dup_str);
+        free(new_node);
         return (NULL);
     }
 
     new_node->str = dup_str;
-    new_node->len = len;
-    new_node->next = *head;
+    new_node->next = NULL;
 
+    if (!*head)
+    {
+        *head = new_node;
+        return (new_node);
+    }
 
-    *head = new_node;
+    temp = *head;
+    while (temp->next)
+        temp = temp->next;
+
+    temp->next = new_node;
 
     return (new_node);
 }
