24e649431c20a81ba5e15c76168878bdb3e4b5f5
CISCO2020 <CISCO2020@GG-MACHINE>
2025-04-06 14:30:03 +1000

    add

---
+++ doubly_linked_lists/lists.h
@@ -0,0 +1,29 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct dlistint_s - Doubly linked list node structure
+ * @n: Integer value stored in the node
+ * @prev: Pointer to the previous node
+ * @next: Pointer to the next node
+ * Description: Doubly linked list structure for Holberton project
+ */
+typedef struct dlistint_t
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t** head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t** head, const int n);
+void free_dlistint(dlistint_t *head);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+size_t dlistint_len(const dlistint_t *h);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+
+#endif
